51c181d19d62b8ee9ad7a3500b0c396f0f039302
washington1313 <123412352+washington1313@users.noreply.github.com>
2023-04-05 22:22:28 -0300

    Add files via upload

---
+++ 2 Vetores/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numerosPares = new int[10];
+            int[] numerosImpares = new int[10];
+
+            int numero;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Digite o Número:");
+                numero = int.Parse(Console.ReadLine());
+                Console.Clear();
+
+                if (numero % 2 == 0)
+                {
+                    numerosPares[i] = numero;
+                }
+
+                else if (numero % 2 == 1)
+                {
+                    numerosImpares[i] = numero;
+                }
+            }
+
+            Console.WriteLine("Números Pares: ");
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine(numerosPares[i]);
+            }
+
+            Console.WriteLine("Números Ímpares");
+            for (int i = 0; i < 10; i++) 
+            {
+                Console.WriteLine(numerosImpares[i]);
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
